d2e02d6f71946b879bd26ebc9867e59788009376
Kushal292000 <kushalthapa665@gmail.com>
2026-02-28 14:48:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -118,3 +118,31 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 };
+
+/* =========================
+   NAVBAR ACTIVE SCROLL
+========================= */
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+
+        if (window.pageYOffset >= sectionTop - 150) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+});
